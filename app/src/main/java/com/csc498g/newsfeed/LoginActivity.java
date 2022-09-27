0ac14bc27378a7bcd6272edeaa6c54d6410baa0c
Simon <moawadsimon@gmail.com>
2022-09-27 15:36:38 +0300

    Retrieved the username and stored it in the shared preferences

--- app/src/main/java/com/csc498g/newsfeed/LoginActivity.java
+++ app/src/main/java/com/csc498g/newsfeed/LoginActivity.java
@@ -5,6 +5,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -15,6 +18,13 @@ public class LoginActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         sp = this.getSharedPreferences("com.csc498g.newsfeed", Context.MODE_PRIVATE);
+
+    }
+
+    public void login(View v) {
+
+        EditText loginEdit = findViewById(R.id.loginEdit);
+        sp.edit().putString("username", loginEdit.getText().toString()).apply();
 
     }
 }
