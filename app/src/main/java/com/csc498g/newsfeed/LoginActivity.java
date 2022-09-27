d947de2d563a9ab55a37d3f7deba57696aee0056
Simon <moawadsimon@gmail.com>
2022-09-27 15:29:16 +0300

    Created shared preferences

--- app/src/main/java/com/csc498g/newsfeed/LoginActivity.java
+++ app/src/main/java/com/csc498g/newsfeed/LoginActivity.java
@@ -2,13 +2,19 @@ package com.csc498g.newsfeed;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
 public class LoginActivity extends AppCompatActivity {
 
+    SharedPreferences sp;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        sp = this.getSharedPreferences("com.csc498g.newsfeed", Context.MODE_PRIVATE);
+
     }
 }
