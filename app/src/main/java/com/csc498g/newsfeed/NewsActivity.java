b326371e183806d7dfc3f13bb6dd6fa6e07c45b9
Simon <moawadsimon@gmail.com>
2022-09-28 12:49:23 +0300

    Cleaned the code with some constants

--- app/src/main/java/com/csc498g/newsfeed/NewsActivity.java
+++ app/src/main/java/com/csc498g/newsfeed/NewsActivity.java
@@ -18,6 +18,22 @@ public class NewsActivity extends AppCompatActivity {
     ListView news;
     ArrayList<String> newsContent;
     SQLiteDatabase sql;
+
+    enum TABLE_COLUMNS {
+
+        AUTHOR("author", 0), HEADLINE("headline", 1), DESCRIPTION("description", 2), PUBLISHED_AT("published_at", 3), LOCATION("location", 4);
+
+        final String label;
+        final int index;
+
+        TABLE_COLUMNS(String label, int index) {
+            this.label = label;
+            this.index = index;
+        }
+
+
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -42,22 +58,22 @@ public class NewsActivity extends AppCompatActivity {
 
         ArrayList<HashMap<String, String>> result = new ArrayList<>();
         Cursor c = sql.rawQuery("Select * from students", null);
-        int author_id = c.getColumnIndex("headline");
-        int headline_id = c.getColumnIndex("description");
-        int description_id = c.getColumnIndex("description");
-        int published_at_id = c.getColumnIndex("published_at");
-        int location_id = c.getColumnIndex("location");
+//        int author_id = c.getColumnIndex("headline");
+//        int headline_id = c.getColumnIndex("description");
+//        int description_id = c.getColumnIndex("description");
+//        int published_at_id = c.getColumnIndex("published_at");
+//        int location_id = c.getColumnIndex("location");
 
         c.moveToFirst();
 
         while(c != null) {
 
             HashMap<String, String> row = new HashMap<>();
-            row.put("author", c.getString(author_id));
-            row.put("headline", c.getString(headline_id));
-            row.put("description", c.getString(description_id));
-            row.put("published_at", c.getString(published_at_id));
-            row.put("location", c.getString(location_id));
+            row.put(TABLE_COLUMNS.AUTHOR.label, c.getString(TABLE_COLUMNS.AUTHOR.index));
+            row.put(TABLE_COLUMNS.HEADLINE.label, c.getString(TABLE_COLUMNS.HEADLINE.index));
+            row.put(TABLE_COLUMNS.DESCRIPTION.label, c.getString(TABLE_COLUMNS.DESCRIPTION.index));
+            row.put(TABLE_COLUMNS.PUBLISHED_AT.label, c.getString(TABLE_COLUMNS.PUBLISHED_AT.index));
+            row.put(TABLE_COLUMNS.LOCATION.label, c.getString(TABLE_COLUMNS.LOCATION.index));
             c.moveToNext();
             result.add(row);
 
