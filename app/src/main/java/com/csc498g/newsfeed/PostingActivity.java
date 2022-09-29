9feac68ef79df3a2d3fdc899bda535ef567dc88e
Simon <moawadsimon@gmail.com>
2022-09-29 22:49:53 +0300

    Prepared database insertion

--- app/src/main/java/com/csc498g/newsfeed/PostingActivity.java
+++ app/src/main/java/com/csc498g/newsfeed/PostingActivity.java
@@ -1,14 +1,28 @@
 package com.csc498g.newsfeed;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 public class PostingActivity extends AppCompatActivity {
+
+    News news;
+    SQLiteDatabase sql;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_posting);
+        news = new News();
+
+        sql = this.openOrCreateDatabase("newsfeeddb", MODE_PRIVATE, null);
+        sql.execSQL("INSERT INTO students(author, headline, description, published_at, location) VALUES (?, ?, ?, ?, ?)", new String[]{
+                news.getAuthor(),
+                news.getHeadline(),
+                news.getDescription(),
+                news.getPublished_at(),
+                news.getLocation()
+        });
     }
 }
