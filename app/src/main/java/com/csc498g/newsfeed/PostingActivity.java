cf1403f5fd85edf8c3bffe80647bdd0d5b817f30
Simon <moawadsimon@gmail.com>
2022-09-29 22:53:33 +0300

    Handled posting

--- app/src/main/java/com/csc498g/newsfeed/PostingActivity.java
+++ app/src/main/java/com/csc498g/newsfeed/PostingActivity.java
@@ -2,6 +2,8 @@ package com.csc498g.newsfeed;
 
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -14,7 +16,12 @@ public class PostingActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_posting);
-        news = new News();
+        news = retrieveData();
+
+
+    }
+
+    public void postNews(View view) {
 
         sql = this.openOrCreateDatabase("newsfeeddb", MODE_PRIVATE, null);
         sql.execSQL("INSERT INTO students(author, headline, description, published_at, location) VALUES (?, ?, ?, ?, ?)", new String[]{
@@ -24,5 +31,18 @@ public class PostingActivity extends AppCompatActivity {
                 news.getPublished_at(),
                 news.getLocation()
         });
+
+    }
+
+    private News retrieveData() {
+
+        News news = new News();
+        news.setAuthor(((EditText)findViewById(R.id.authorEdit)).getText().toString());
+        news.setHeadline(((EditText)findViewById(R.id.headlingEdit)).getText().toString());
+        news.setDescription(((EditText)findViewById(R.id.descriptionEdit)).getText().toString());
+        news.setPublished_at(((EditText)findViewById(R.id.dateEdit)).getText().toString());
+        news.setLocation(((EditText)findViewById(R.id.locationEdit)).getText().toString());
+        return news;
+
     }
 }
