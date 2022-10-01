658a02faeb53275684cb826e308f32e89e15f26f
Simon <moawadsimon@gmail.com>
2022-10-01 15:55:52 +0300

    Populated the update layout with the existing news

--- app/src/main/java/com/csc498g/newsfeed/UpdateActivity.java
+++ app/src/main/java/com/csc498g/newsfeed/UpdateActivity.java
@@ -3,6 +3,7 @@ package com.csc498g.newsfeed;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class UpdateActivity extends AppCompatActivity {
 
@@ -21,5 +22,18 @@ public class UpdateActivity extends AppCompatActivity {
         news.setPublished_at(getIntent().getStringExtra(TABLE_COLUMNS.PUBLISHED_AT.label));
         news.setLocation(getIntent().getStringExtra(TABLE_COLUMNS.LOCATION.label));
 
+        populateData();
+
     }
+
+    private void populateData() {
+
+        ((EditText)findViewById(R.id.authorEdit)).setText(news.getAuthor());
+        ((EditText)findViewById(R.id.headlingEdit)).setText(news.getHeadline());
+        ((EditText)findViewById(R.id.descriptionEdit)).setText(news.getDescription());
+        ((EditText)findViewById(R.id.dateEdit)).setText(news.getPublished_at());
+        ((EditText)findViewById(R.id.locationEdit)).setText(news.getLocation());
+
+    }
+
 }
