be2b25dc56ed5fb3afad3fb4bb56a5df0f8531f9
Simon <moawadsimon@gmail.com>
2022-10-01 15:37:00 +0300

    Created an Updating activity. Reused Layout from Posting Activity

---
+++ app/src/main/java/com/csc498g/newsfeed/UpdateActivity.java
@@ -0,0 +1,15 @@
+package com.csc498g.newsfeed;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class UpdateActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_updating);
+    }
+}
